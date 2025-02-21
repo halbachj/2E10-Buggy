84708ab4803de27dd23d065c48d78f1e118c9f23
johannes@halbachnet.de <johannes@halbachnet.de>
2025-02-21 08:58:28 +0000

    working on line following with motor pid control

--- LineFollower.cpp
+++ LineFollower.cpp
@@ -9,19 +9,13 @@ void LineFollower::update() {
   float error = this->leftSensor.getManualReading() - this->rightSensor.getManualReading();
   float per_err = (abs(error) / 1023);
 
-  if (per_err < 0.20) per_err = 0;
 
+  // Dead band
+  if (per_err < 0.20) per_err = 0;
   if (per_err > 0.50) per_err = 1.0; 
 
   int set_speed = this->target_speed * (1 - 1.1 * per_err);
   set_speed = constrain(set_speed, 0, 1000);
-
-  /*if (this->leftSensor.getManualReading() > 800)
-  {
-    this->leftMotor.pwmOverride(0);
-  } else if (this->rightSensor.getManualReading() > 800) {
-    this->rightMotor.pwmOverride(0);
-  } else*/
 
   if (error < 0) {
     //this->leftMotor.pwmOverride(round(set_speed));
