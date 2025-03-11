916ef13497b6c04dc76da3f7348fde378a064ac2
johannes@halbachnet.de <johannes@halbachnet.de>
2025-03-11 10:16:55 +0000

    fixed rebase issues

--- BuggySoftware/LineFollowingState.cpp
+++ BuggySoftware/LineFollowingState.cpp
@@ -16,17 +16,9 @@ void LineFollowingState::update(Buggy& buggy, double dt) {
 
   buggy.objectDetected = buggy.ultrasonicSensor.objectDetected();
   if (buggy.objectDetected) {
-<<<<<<< HEAD
-    buggy.setState(ObjectDetectedState::instance());
-=======
     buggy.setState(ObjectDetectedHandlerState::instance());
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
   }
 }
 
 void LineFollowingState::exit(Buggy& buggy, BuggyState* oldState) {
-<<<<<<< HEAD
-  // Cleanup logic before leaving idle state
-=======
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
 }
--- BuggySoftware/ObjectDetectedState.cpp
+++ BuggySoftware/ObjectDetectedState.cpp
@@ -2,17 +2,10 @@
 #include "EmbeddedLogger.hpp"
 
 using EmbeddedLogger::logger;
-<<<<<<< HEAD
-
-
-void ObjectDetectedState::enter(Buggy& buggy, BuggyState* oldState) {
-  // Setup logic when entering idle state
-=======
 using logLevel = EmbeddedLogger::LogLevel;
 
 
 void ObjectDetectedState::enter(Buggy& buggy, BuggyState* oldState) {
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
   this->left_old_pwm = buggy.leftMotor.getSpeed();
   this->right_old_pwm = buggy.rightMotor.getSpeed();
   buggy.leftMotor.pwmOverride(0);
@@ -23,24 +16,14 @@ void ObjectDetectedState::enter(Buggy& buggy, BuggyState* oldState) {
 void ObjectDetectedState::update(Buggy& buggy, double dt) {
   buggy.ultrasonicSensor.update();
   float distance = buggy.ultrasonicSensor.getReading();
-<<<<<<< HEAD
-  //logger << distance << EmbeddedLogger::endl;
-=======
   logger << logLevel::INFO << "Object Distance: " << distance << EmbeddedLogger::endl;
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
   buggy.objectDetected = buggy.ultrasonicSensor.objectDetected();
   if (!buggy.objectDetected) { 
     buggy.setState(LineFollowingState::instance());
   }
 }
 
-<<<<<<< HEAD
-void ObjectDetectedState::exit(Buggy& buggy, BuggyState* oldState) {
-  // Cleanup logic before leaving idle state
-}
-=======
 void ObjectDetectedState::exit(Buggy& buggy, BuggyState* oldState) {}
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
 
 
 /*
@@ -48,17 +31,6 @@ void ObjectDetectedState::exit(Buggy& buggy, BuggyState* oldState) {}
  */
 
 void ObjectDetectedHandlerState::enter(Buggy& buggy, BuggyState* oldState) {
-<<<<<<< HEAD
-  this->oldState = oldState;
-}
-
-void ObjectDetectedHandlerState::update(Buggy& buggy, double dt) {
-  buggy.setState(ObjectDetectedState::instance());
-}
-
-void ObjectDetectedHandlerState::exit(Buggy& buggy, BuggyState* oldState) {
-  buggy.setState(*this->oldState);
-=======
   this->savedState = oldState;
   if (oldState != &ObjectDetectedState::instance()) { // entering from different state / going to stop. Can compare because state is singleton
     buggy.setState(ObjectDetectedState::instance());
@@ -71,5 +43,4 @@ void ObjectDetectedHandlerState::exit(Buggy& buggy, BuggyState* oldState) {
   if (oldState == &ObjectDetectedState::instance()) {
     buggy.setState(*this->savedState);
   }
->>>>>>> c3ac23c (fixed the handler state for object detected and added an experimental udp server)
 }
