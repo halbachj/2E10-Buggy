51f6352c83ebc08c9c2ebf56df45e845817e4db3
halbachj <24858624+halbachj@users.noreply.github.com>
2025-03-12 09:34:09 +0000

    Deploying to gh-pages from @ halbachj/2E10-Buggy@8cefb163b71f458028084de7df8ff58e180f6003 🚀

--- search/all_c.js
+++ search/all_c.js
@@ -4,5 +4,6 @@ var searchData=
   ['motordriver_1',['MotorDriver',['../classMotorDriver.html',1,'MotorDriver'],['../classMotorDriver.html#aa5a9331e942968efe9b13f643d04fcfd',1,'MotorDriver::MotorDriver(const MotorPinGroup &amp;pinGroup, const PIDConstants &amp;constants)'],['../classMotorDriver.html#a56e9308b5207b35e9477a9b42169c735',1,'MotorDriver::MotorDriver(const MotorDriver &amp;other)=delete']]],
   ['motorpingroup_2',['MotorPinGroup',['../structMotorPinGroup.html',1,'']]],
   ['movemeanfilter_3',['MoveMeanFilter',['../classMoveMeanFilter.html',1,'']]],
-  ['movemeanfilter_3c_20unsigned_20int_2c_20filter_5fwindow_20_3e_4',['MoveMeanFilter&lt; unsigned int, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]]
+  ['movemeanfilter_3c_20float_2c_20filter_5fwindow_20_3e_4',['MoveMeanFilter&lt; float, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]],
+  ['movemeanfilter_3c_20unsigned_20int_2c_20filter_5fwindow_20_3e_5',['MoveMeanFilter&lt; unsigned int, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['motordriver_1',['MotorDriver',['../classMotorDriver.html',1,'']]],
   ['motorpingroup_2',['MotorPinGroup',['../structMotorPinGroup.html',1,'']]],
   ['movemeanfilter_3',['MoveMeanFilter',['../classMoveMeanFilter.html',1,'']]],
-  ['movemeanfilter_3c_20unsigned_20int_2c_20filter_5fwindow_20_3e_4',['MoveMeanFilter&lt; unsigned int, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]]
+  ['movemeanfilter_3c_20float_2c_20filter_5fwindow_20_3e_4',['MoveMeanFilter&lt; float, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]],
+  ['movemeanfilter_3c_20unsigned_20int_2c_20filter_5fwindow_20_3e_5',['MoveMeanFilter&lt; unsigned int, filter_window &gt;',['../classMoveMeanFilter.html',1,'']]]
 ];
