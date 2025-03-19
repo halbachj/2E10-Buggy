11d8de67a09b5018b1d8c62603424afe9dacf03a
johannes@halbachnet.de <johannes@halbachnet.de>
2025-03-19 16:57:45 +0000

    silver demo

--- BuggySoftware/LineFollower.hpp
+++ BuggySoftware/LineFollower.hpp
@@ -23,8 +23,8 @@ private:
   PIDController controller; // Initialize the PID with the right coefficients
 
   int target_speed = 400; // 160
-  int turning_threshold = 125;
-  int turning_speed = 100;
+  int turning_threshold = 135;
+  int turning_speed = 125;
 
 public:
   /**
